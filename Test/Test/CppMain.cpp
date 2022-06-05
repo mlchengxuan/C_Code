1cd525313b9e8d53d99988a4d0f22fa056ed83f8
mlchengxuan <mlchengxuan@163.com>
2022-06-05 20:34:29 +0800

    起始代码上传

---
+++ Test/Test/CppMain.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<string>
+using namespace std;
+//函数=首字母大写
+//变量=驼峰命名法-前小后大
+//
+
+
+
+int main()
+{
+ //char  //字符 有256个
+	char testchar = 'Y ';//字符串""
+	cout << testchar << endl;
+	cout << sizeof(int) << endl;
+
+	return 0;
+
+
+}
