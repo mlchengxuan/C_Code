63672ffde9642a8f2b3e05f2891ceaf7d8c62d9c
mlchengxuan <mlchengxuan@163.com>
2022-06-05 21:33:40 +0800

    01

--- Test/Test/CppMain.cpp
+++ Test/Test/CppMain.cpp
@@ -3,16 +3,22 @@
 using namespace std;
 //函数=首字母大写
 //变量=驼峰命名法-前小后大
-//
+//%c打印字符  %s打印字符串 %d打印整型 
+//printf("%d",100);
+//printf("%s","abc");
+//printf("%c",'a');
+	
 
 
 
 int main()
 {
+	//转义字符 \ 转变了它原来的意思
+	printf("%c\n",'\101');
  //char  //字符 有256个
-	char testchar = 'Y ';//字符串""
-	cout << testchar << endl;
-	cout << sizeof(int) << endl;
+	//char testchar = 'Y ';//字符串""
+	//cout << testchar << endl;
+	//cout << sizeof(int) << endl;
 
 	return 0;
 
